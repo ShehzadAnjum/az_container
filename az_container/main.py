911eba3c83ead8146b3bd00fbb16fbb6ec088c54
ShehzadAnjum <sanjum77@gmail.com>
2024-07-06 04:15:23 +0500

    c2

--- az_container/main.py
+++ az_container/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app = FastAPI(title="AZURE Container -- Hello World ! Testing --", 
+    version="0.0.1",
+    # servers=[
+    #     {
+    #         "url": "http://127.0.0.1:8001", # ADD NGROK URL Here Before Creating GPT Action
+    #         "description": "Development Server"
+    #     }
+    #     ]
+    )
+
+
 @app.get("/")
 def index():
-    return({"message":"Welcome to the Azure container world !"})
+    return({"message":"Welcome to the Azure container world ! The final version"})
 
 @app.get("/name")
 def name():
