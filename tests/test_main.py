c6a38fa4a04fe41e85a6429cafca95d8a36425ed
ShehzadAnjum <sanjum77@gmail.com>
2024-07-05 17:52:59 +0500

    C1

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi.testclient import TestClient
+from az_container.main import app
+
+client  = TestClient(app)
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message":"Welcome to the Azure container world !"}
+
+def test_name():
+    response = client.get("/name")
+    assert response.status_code == 200
+    assert response.json() == {"name":"Shehzad Anjum"}
+
+def test_batch():
+    response = client.get("/batch")
+    assert response.status_code ==200
+    assert response.json() == {"batch":"36-37 Cloud computing and GenAI "}
